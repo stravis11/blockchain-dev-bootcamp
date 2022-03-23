e96924d3ac50e3959bd1841853e037dc34668a7e
Steve Travis <stevetravis11@gmail.com>
2022-03-23 10:39:56 -0400

    Tests passed.

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -2,5 +2,13 @@
 pragma solidity ^0.5.0;
 
 contract Token {
-  string public name = "My Name";  
+  string public name = "DApp Token";  
+  string public symbol = "DAPP";
+  uint256 public decimals = 18;
+  uint256 public totalSupply;
+
+
+  constructor() public {
+    totalSupply = 1000000 * (10 ** decimals);
+  }
 }
